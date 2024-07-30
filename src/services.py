3401bd06a5088b43c37d40c4a4f3f2348dc1da27
valerie synenko <valerii.synenko@gmail.com>
2024-07-30 22:16:59 +0300

    Improve test for user registration. Delete test for user creation

---
+++ src/services.py
@@ -0,0 +1,23 @@
+import json
+
+import requests
+
+
+class ComonServices:
+
+    def __init__(self):
+        self.base_url = "https://reqres.in/api"
+        self.request_headers = {"Content-Type": "application/json"}
+
+    def perform_post_requests(self, endpoint: str, user_payload: dict) -> requests.Response:
+        return requests.post(
+            url=f"{self.base_url}{endpoint}",
+            headers=self.request_headers,
+            data=json.dumps(user_payload),
+        )
+
+
+class UserApiServices(ComonServices):
+
+    def register_new_user(self, user_payload) -> requests.Response:
+        return self.perform_post_requests(endpoint="/register", user_payload=user_payload)
--- tests/test_user_registration.py
+++ tests/test_user_registration.py
@@ -2,32 +2,15 @@ import json
 
 import requests
 
-
-def test_can_create_new_user():
-    """
-    Test for creation of a new user
-    """
-    service_url = "https://reqres.in/"
-
-    user_request_body = {"name": "valerie", "job": "AQA"}
-
-    response = requests.post(
-        url=f"{service_url}/api/user", headers={"Content-Type": "application/json"}, data=json.dumps(user_request_body)
-    )
-
-    assert response.status_code == 201
+from src.services import UserApiServices
 
 
 def test_can_register_new_user():
     """
     Test for registration of a user
     """
-    user_request_body = {"email": "eve.holt@reqres.in", "password": "cityslicka"}
-
-    response = requests.post(
-        url="https://reqres.in/api/register",
-        headers={"Content-Type": "application/json"},
-        data=json.dumps(user_request_body),
+    user_registration_response = UserApiServices().register_new_user(
+        {"email": "eve.holt@reqres.in", "password": "cityslicka"}
     )
 
-    assert response.status_code == 200
+    assert user_registration_response.status_code == 200
