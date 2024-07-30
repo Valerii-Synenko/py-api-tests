6198439706c7d243def6ecafa45b246ca7e52cfa
valerie synenko <valerii.synenko@gmail.com>
2024-07-30 21:52:26 +0300

    Fix test for user registration

--- tests/test_user_registration.py
+++ tests/test_user_registration.py
@@ -4,6 +4,9 @@ import requests
 
 
 def test_can_create_new_user():
+    """
+    Test for creation of a new user
+    """
     service_url = "https://reqres.in/"
 
     user_request_body = {"name": "valerie", "job": "AQA"}
@@ -16,20 +19,15 @@ def test_can_create_new_user():
 
 
 def test_can_register_new_user():
-    user_request_body = {
-        "email": "val.erie@reqres.in",
-        "password": "StrongPWD"
-    }
+    """
+    Test for registration of a user
+    """
+    user_request_body = {"email": "eve.holt@reqres.in", "password": "cityslicka"}
 
     response = requests.post(
         url="https://reqres.in/api/register",
         headers={"Content-Type": "application/json"},
-        data=json.dumps(user_request_body)
+        data=json.dumps(user_request_body),
     )
 
-    print(response)
-
     assert response.status_code == 200
-
-
-
