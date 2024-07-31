54023db9c36ed32c45ff45fa0fafb294e4fa3c91
valerie synenko <valerii.synenko@gmail.com>
2024-07-31 17:36:24 +0300

    feat: Implement fixtures for

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,14 @@
+import pytest
 from faker import Faker
 
+from src.services import UserApiServices
 
+
+@pytest.fixture
 def faker():
     return Faker()
+
+
+@pytest.fixture
+def user_ppi_services():
+    return UserApiServices()
