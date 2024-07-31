306abbbb4a328c1b48747e09d3a7854b77144af0
valerie synenko <valerii.synenko@gmail.com>
2024-07-31 17:36:54 +0300

    refactor: Use fixture

--- tests/test_user_registration.py
+++ tests/test_user_registration.py
@@ -1,11 +1,10 @@
-from src.services import UserApiServices
 
 
-def test_can_register_new_user():
+def test_can_register_new_user(user_ppi_services):
     """
     Test for registration of a user
     """
-    user_registration_response = UserApiServices().register_new_user(
+    user_registration_response = user_ppi_services.register_new_user(
         user_email="eve.holt@reqres.in",
         user_password="cityslicka",
     )
@@ -13,13 +12,13 @@ def test_can_register_new_user():
     assert user_registration_response.status_code == 200
 
 
-def test_can_create_new_user():
+def test_can_create_new_user(user_ppi_services, faker):
     """
     Test for creation of a user
     """
-    user_creation_response = UserApiServices().create_new_user(
-        user_name="morpheus",
-        user_jod="leader",
+    user_creation_response = user_ppi_services.create_new_user(
+        user_name=faker.name(),
+        user_jod=faker.job(),
     )
 
     assert user_creation_response.status_code == 201
