044f892f72dfa7b030ee4642acadb17c8c1e0452
valerie synenko <valerii.synenko@gmail.com>
2024-07-31 17:16:49 +0300

    refactor: Improve test logic due to new service methods

--- tests/test_user_registration.py
+++ tests/test_user_registration.py
@@ -6,7 +6,8 @@ def test_can_register_new_user():
     Test for registration of a user
     """
     user_registration_response = UserApiServices().register_new_user(
-        {"email": "eve.holt@reqres.in", "password": "cityslicka"}
+        user_email="eve.holt@reqres.in",
+        user_password="cityslicka",
     )
 
     assert user_registration_response.status_code == 200
@@ -16,8 +17,9 @@ def test_can_create_new_user():
     """
     Test for creation of a user
     """
-
-    user_creation_response = UserApiServices().create_new_user(user_payload={"name": "morpheus", "job": "leader"})
+    user_creation_response = UserApiServices().create_new_user(
+        user_name="morpheus",
+        user_jod="leader",
+    )
 
     assert user_creation_response.status_code == 201
-
