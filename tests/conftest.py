f5b7fff2f4fee4945a83f1efb769dc9e9037ec69
valerie synenko <valerii.synenko@gmail.com>
2024-08-03 00:18:21 +0300

    refactor: Add docstring for faker and user_ppi_services fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,9 +6,18 @@ from src.services import UserApiServices
 
 @pytest.fixture
 def faker():
+    """
+    The fixture that provides the object of the Faker.
+    The Fake is the object needs to generate of randomly fake data.
+    :return: The Faker object
+    """
     return Faker()
 
 
 @pytest.fixture
 def user_ppi_services():
+    """
+    The fixture that provides the object of the UserApiServices class
+    :return: the object of the UserApiServices class
+    """
     return UserApiServices()
