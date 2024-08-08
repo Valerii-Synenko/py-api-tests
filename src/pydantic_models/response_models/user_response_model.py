9e603e601f3f55614adc5c15a426dc57c8fc179c
valerie synenko <valerii.synenko@gmail.com>
2024-08-08 16:10:39 +0300

    feat: Add field validators for CreateUserResponseModel class

--- src/pydantic_models/response_models/user_response_model.py
+++ src/pydantic_models/response_models/user_response_model.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel
+from datetime import datetime
+
+from pydantic import BaseModel, field_validator
 
 
 class CreateUserResponseModel(BaseModel):
@@ -6,6 +8,32 @@ class CreateUserResponseModel(BaseModel):
     job: str
     id: str
     createdAt: str
+
+    @field_validator("name")
+    def check_name(cls, value):
+        if not isinstance(value, str) or len(value) < 0:
+            raise ValueError('"name" must be a string with at least 1 length')
+        return value
+
+    @field_validator("job")
+    def check_name(cls, value):
+        if not isinstance(value, str) or len(value) < 0:
+            raise ValueError('"job" must be a string with at least 1 length')
+        return value
+
+    @field_validator("id")
+    def check_name(cls, value):
+        if not isinstance(value, str) or len(value) < 0:
+            raise ValueError('"id" must be a string with at least 1 length')
+        return value
+
+    @field_validator("createdAt")
+    def validate_created_at(cls, value):
+        try:
+            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%fZ")
+        except ValueError:
+            raise ValueError('"createdAt" must be in the format YYYY-MM-DDTHH:MM:SS.ffffffZ')
+        return value
 
 
 class RegisterUserResponseModel(BaseModel):
