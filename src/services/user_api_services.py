1821b7fb0d6ee6e5bcf794fc38d69f65cf064291
valerie synenko <valerii.synenko@gmail.com>
2024-08-08 16:57:36 +0300

    refactor: Rename params

--- src/services/user_api_services.py
+++ src/services/user_api_services.py
@@ -12,29 +12,29 @@ from src.services.common_service import ComonServices
 
 class UserApiServices(ComonServices):
 
-    def register_new_user(self, user_email: str, user_password: str) -> tuple[int, RegisterUserResponseModel]:
+    def register_new_user(self, email: str, password: str) -> tuple[int, RegisterUserResponseModel]:
         """
         Method make registration of a new user.
         It makes the POST method to the "/register" endpoint, with the specific data
-        :param user_email: The email with which will be registered a user
-        :param user_password: The password with which the user will be registered
+        :param email: The email with which will be registered a user
+        :param password: The password with which the user will be registered
         :return: Response status code and RegisterUserResponseModel
         """
-        register_user_request_model = RegisterUserRequestModel(email=user_email, password=user_password)
+        register_user_request_model = RegisterUserRequestModel(email=email, password=password)
         response = self.perform_post_requests(endpoint="/register", payload=register_user_request_model.model_dump())
         register_user_response_model = RegisterUserResponseModel(**response.json())
 
         return response.status_code, register_user_response_model
 
-    def create_new_user(self, user_name: str, user_job: str) -> tuple[int, CreateUserResponseModel]:
+    def create_new_user(self, name: str, job: str) -> tuple[int, CreateUserResponseModel]:
         """
         Method makes creation/addition of a new user.
         It makes the POST method to the "/user" endpoint, with the specific data
-        :param user_name: The name with which will be created a user
-        :param user_job: The user's job with which the user will be created
+        :param name: The name with which will be created a user
+        :param job: The user's job with which the user will be created
         :return: Tuple of two elements, CreateUserResponse pydantic model and response status code.
         """
-        create_user_request_model = CreateUserRequestModel(name=user_name, job=user_job)
+        create_user_request_model = CreateUserRequestModel(name=name, job=job)
         response = self.perform_post_requests(endpoint="/users", payload=create_user_request_model.model_dump())
         create_user_response_model = CreateUserResponseModel(**response.json())
 
