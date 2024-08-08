02f2d95f780f528dceef42ec09ad3dce759861b3
valerie synenko <valerii.synenko@gmail.com>
2024-08-08 17:41:39 +0300

    feat: Add validator for the email field in the RegisterUserRequestModel

--- src/pydantic_models/request_models/user_request_model.py
+++ src/pydantic_models/request_models/user_request_model.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
 class CreateUserRequestModel(BaseModel):
@@ -9,3 +9,18 @@ class CreateUserRequestModel(BaseModel):
 class RegisterUserRequestModel(BaseModel):
     email: str
     password: str
+
+    @field_validator("email")
+    def validate_email(cls, value):
+        specified_emails = [
+            "george.bluth@reqres.in",
+            "janet.weaver@reqres.in",
+            "emma.wong@reqres.in",
+            "eve.holt@reqres.in",
+            "charles.morris@reqres.in",
+            "tracey.ramos@reqres.in",
+        ]
+
+        if value not in specified_emails:
+            raise ValueError(f"'email' must be one of the member of the {specified_emails}")
+        return value
